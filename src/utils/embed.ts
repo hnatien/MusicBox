0c1bf944d705b4ebe0dd2df18ecfad31a8bbd0da
hnatien <hoangnhattien2307@gmail.com>
2026-03-31 23:14:06 +0700

    feat: update now playing embed author icon to music notes

--- src/utils/embed.ts
+++ src/utils/embed.ts
@@ -24,7 +24,7 @@ export function createNowPlayingEmbed(song: Song, elapsedSeconds: number, isPaus
 
     const embed = new EmbedBuilder()
         .setColor(0x2B2D31)
-        .setAuthor({ name: song.channelName.toUpperCase(), iconURL: getEmojiUrl(APP_EMOJIS.WAVES) })
+        .setAuthor({ name: song.channelName.toUpperCase(), iconURL: getEmojiUrl(APP_EMOJIS.MUSIC_NOTES) })
         .setTitle(song.title)
         .setURL(song.url)
         .setImage(song.thumbnail || null)
